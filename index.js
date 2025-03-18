b4f0b736fe4946d5eebe5e63ae374e56c59f4fed
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-03-19 01:00:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// import module
 import { savefrom } from '@bochilteam/scraper-savefrom'
 
-// Fetch data
 const data = await savefrom('https://youtu.be/iik25wqIuFo')
 
-// Pretty print JSON response
-console.log(JSON.stringify(data, null, 2))
+// Saare iTags aur unka info dikhane ke liye:
+data.links.forEach(link => {
+  console.log(`iTag: ${link.itag}, Quality: ${link.quality}, Type: ${link.type}, URL: ${link.url}`)
+})
