dd84a348faeadf400c4eb95ffecf057179232dcc
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-03-18 22:48:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { savefrom } from '@bochilteam/scraper-savefrom'
+// import module
+import { youtubedl } from '@bochilteam/scraper-youtube'
 
-const data = await savefrom('https://youtu.be/iik25wqIuFo')
-console.log(data)
+const data = await youtubedl('https://youtu.be/iik25wqIuFo')
+console.log(data) // JSON
+const resolutions = Object.keys(data.video) // List of resolution/quality
+console.log(resolutions) 
+const url = await data.video[resolutions[0]].download() // Download '720p' video
+console.log(url) // string
